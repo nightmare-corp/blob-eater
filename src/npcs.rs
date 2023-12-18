722827b0a549f897c41298f12045171c964dfd19
archsolar <m_charizard@hotmail.nl>
2023-12-18 12:54:06 +0100

    Refactor NPC spawning and movement

--- src/npcs.rs
+++ src/npcs.rs
@@ -1,79 +1,114 @@
+use std::f32::consts::E;
+
 use crate::characters::CharacterBundle;
 use bevy::prelude::*;
-use rand::Rng;
+use rand::{distributions::Distribution, Rng};
 
 const NPC_COUNT: usize = 100;
 
 #[derive(Component)]
 pub struct Npc {
-    pub direction_id: u8,
+    // pub direction_id: u8,
     pub speed: f32,
+    pub direction: Vec2,
 }
 impl Npc {
-    fn new() -> Self {
+    fn new(direction: Vec2) -> Self {
+        //TODO maybe no need to normalize..
+        let direction = direction.normalize();
         Self {
-            direction_id: rand::thread_rng().gen_range(0..8),
+            // direction_id: rand::thread_rng().gen_range(0..8),
             speed: rand::thread_rng().gen_range(50.0..300.0),
+            direction,
         }
-    }
-}
-impl Default for Npc {
-    fn default() -> Self {
-        Self::new()
     }
 }
 pub struct NpcPlugin;
 
 impl Plugin for NpcPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, npc_setup)
-            .add_systems(Update, (npc_movement, npc_spawn));
+        app.add_systems(Update, (npc_movement, npc_spawn));
     }
 }
-
-fn direction_to_vec(dir_id: u8) -> Vec2 {
-    match dir_id {
-        0 => Vec2::new(0., 1.),
-        1 => Vec2::new(1., 1.),
-        2 => Vec2::new(1., 0.),
-        3 => Vec2::new(1., -1.),
-        4 => Vec2::new(0., -1.),
-        5 => Vec2::new(-1., -1.),
-        6 => Vec2::new(-1., 0.),
-        7 => Vec2::new(-1., 1.),
-        _ => Vec2::new(0., 1.),
+#[derive(Copy, Clone)]
+enum SpawnOrigin {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+//standard distribution for spawn origin so we can use rng.gen()
+impl Distribution<SpawnOrigin> for rand::distributions::Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SpawnOrigin {
+        match rng.gen_range(0..3) {
+            0 => SpawnOrigin::Left,
+            1 => SpawnOrigin::Right,
+            2 => SpawnOrigin::Top,
+            3 => SpawnOrigin::Bottom,
+            _ => SpawnOrigin::Left,
+        }
     }
 }
+///right now reverses the vector
+fn point_to_direction(location: Vec2) -> Vec2 {
+    //direction
+    // Vec2::new(0., 0.)
+    //first reverse the vector
+    let direction = -location;
 
-fn npc_setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let radius: f32 = 10.;
-    //npcs
-    commands.spawn((
-        CharacterBundle {
-            mesh: meshes.add(shape::Circle::new(radius).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::RED)),
-            transform: Transform::from_translation(Vec3::new(-radius, 0., 0.)),
-            ..default()
-        },
-        Npc::default(),
-    ));
+    return direction;
 }
+fn calc_npc_spawn(padding: f32, bounds: Vec2) -> (Vec2, Vec2) {
+    let mut rng = rand::thread_rng();
+
+    //grab the middle of both sides of the screen
+    let x = bounds.x / 2.0;
+    let y = bounds.y / 2.0;
+
+    let origin: SpawnOrigin = rng.gen();
+    println!("origin: {:?}", origin as u8);
+
+    let mut location = Vec2::ZERO;
+    let mut direction = Vec2::ZERO;
+    match origin {
+        SpawnOrigin::Left => {
+            location.x = -x - padding;
+            location.y = rng.gen_range(-y..y);
+
+            //TODO randomize direction towards center/inner screen
+        }
+        SpawnOrigin::Right => {
+            location.x = x + padding;
+            location.y = rng.gen_range(-y..y);
+        }
+        SpawnOrigin::Top => {
+            location.y = y - padding;
+            location.x = rng.gen_range(-x..x);
+        }
+        SpawnOrigin::Bottom => {
+            location.y = -y + padding;
+            location.x = rng.gen_range(-x..x);
+        }
+    }
+    direction = point_to_direction(location);
+    (location, direction)
+}
+
 fn npc_spawn(
     mut commands: Commands,
     query: Query<Entity, With<Npc>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    windows: Query<&Window>,
 ) {
+    let window: &Window = windows.single();
+    let bounds: Vec2 = Vec2::new(window.width(), window.height());
+    let (location, direction) = calc_npc_spawn(10., bounds);
     // TODO radius depends on current score.
     let radius: f32 = 10.;
     let count = query.iter().count();
     // TODO color radomized
     let color = Color::RED;
-    let location = Vec2::new(0., 0.);
     // Direction random.
 
     if count < NPC_COUNT {
@@ -84,7 +119,7 @@ fn npc_spawn(
                 transform: Transform::from_translation(Vec3::new(-radius, location.x, location.y)),
                 ..default()
             },
-            Npc::default(),
+            Npc::new(direction),
         ));
     }
 }
@@ -92,10 +127,12 @@ fn npc_movement(
     time: Res<Time>,
     mut commands: Commands,
     mut npc_position: Query<(Entity, &mut Transform, &Npc)>,
+    mut windows: Query<&Window>,
 ) {
-    let bounds = (2000., 2000.);
+    let window = windows.single();
+    let bounds = (window.width(), window.height());
     for (entity, mut transform, npc) in &mut npc_position {
-        let direction = direction_to_vec(npc.direction_id.clone()).normalize();
+        let direction = npc.direction;
 
         transform.translation.x += direction.x * time.delta_seconds() * npc.speed;
         transform.translation.y += direction.y * time.delta_seconds() * npc.speed;
