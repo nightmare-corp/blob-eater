95c60d2c5b90f5d261114216c785f15fa76e0ead
archsolar <m_charizard@hotmail.nl>
2023-12-29 12:35:17 +0100

    Remove debug code and added index.html also added Readm.md commands

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins((player::PlayerPlugin, NpcPlugin))
-        .add_plugins((
-            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
-            // RapierDebugRenderPlugin::default(),
-        ))
+        .add_plugins((RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),))
         .add_systems(Startup, setup_foundation)
         .add_systems(Update, keyboard_input)
         .run();
